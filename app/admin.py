c7b3a87b02162890b2cb6fa8a356d680bd8438ba
BrisaTielly <brisatielly12@gmail.com>
2025-02-21 17:33:16 -0300

    feat: Adiciona a entidade Brand no banco de dados, e a associa ao Car

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from app.models import Car, Brand
+# Register your models here for the admin view.
 
-# Register your models here.
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name', )
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value') #O que vai ser mostrado para o admin
+    search_fields = ('model',) #La na tela de admins, vou poder fazer uma pesquisa pelo modelo do carro 
+    
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Car, CarAdmin)
--- app/models.py
+++ app/models.py
@@ -2,10 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
-    model = models.CharField(max_length=200, default="Unknown Model")
-    brand = models.CharField(max_length=200, default="Unknown")
+    model = models.CharField(max_length=200)
+    brand =  models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
+    plate = models.CharField(max_length=10, blank=True, null=True)
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='app/',blank=True, null=True)
+    
+    def __str__(self):
+        return self.model
